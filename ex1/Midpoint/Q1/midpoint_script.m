8f32ac688baa9483426bf8408babe87cd133b09b
xxyypp <yangpuypyp@outlook.com>
2017-03-13 22:16:49 +0000

    update report and create code folder

--- ex1/Midpoint/Q1/midpoint_script.m
+++ ex1/Midpoint/Q1/midpoint_script.m
@@ -8,23 +8,23 @@ steps=100000; %number of steps
 R=0.5; %R = 0.5 Ohm
 L=0.0015; %I = 1.5mH
 
-Vin = @(t) 5.5*heaviside(t); %step signal
-    f = @(t,i) (Vin(t)-R*(i))/L; %Function in
-    [T,IOut] = midpoint(f, t0, tfinal, i0, steps); %function call
+Vin = @(t) 5.5*heaviside(t); %step the signal
+    f = @(t,i) (Vin(t)-R*(i))/L; %Function given in the question
+    [T,IOut] = midpoint(f, t0, tfinal, i0, steps); %call the function
 
-Vout = zeros(1, steps+1); 
+Vout = zeros(1, steps+1); %initialise Vout
     for j=1:steps
-        Vout(j) = Vin(T(j)) - R*IOut(j); %Create Vout array from Iout, R and Vin
+        Vout(j) = Vin(T(j)) - R*IOut(j); %Use iteration to get Vout
     end
     
-figure %plot graphs
-plot(T, Vout , 'r');
+figure %start to plot the graph
+plot(T, Vout , 'r'); %plot time T vs Vout in red colour
 hold on;
-plot(T, Vin(T), 'g');
-plot(T, IOut, 'b');
+plot(T, Vin(T), 'g'); %plot time T vs Vin in green
+plot(T, IOut, 'b'); %plot time T vs Iout in red colour
     
-legend('Vout(V)', 'Vin(V)', 'IOut(A)') %label
-title('Midpoint Step Signal')%set title
-xlabel('Time /s')%set y,x-axis
+legend('Vout(V)', 'Vin(V)', 'IOut(A)') %label the graph
+title('Midpoint Step Signal')%set the title
+xlabel('Time /s')%set y,x-axis and unit
 ylabel('Amp')
     
---
+++ ex1/code/heun_script.m
@@ -0,0 +1,30 @@
+close all
+clc
+
+t0=0; % t start
+tf=1; %t final
+i0=0;
+step=100000; %number of steps
+R=0.5; %R = 0.5 Ohm
+L=0.0015; %I = 1.5mH
+
+Vin = @(t) 5.5*heaviside(t); %step signal
+    f = @(t,i) (Vin(t)-R*(i))/L; %Function in the question
+    [T,IOut] = heun(f, t0, tf, i0, step); %function call
+
+Vout = zeros(1, step+1); %initialise Vout = 0
+    for j=1:step
+        Vout(j) = Vin(T(j)) - R*IOut(j); %Use iteration to get Vout
+    end
+    
+figure %start to plot the graph
+plot(T, Vout , 'r');%plot time T vs Vout in red colour
+hold on;
+plot(T, Vin(T), 'g');%plot time T vs Vin in green
+plot(T, IOut, 'b');%plot time T vs Iout in blue colour
+    
+legend('Vout(V)', 'Vin(V)', 'IOut(A)') %label the graph
+title('Heun Step Signal')%set the title
+xlabel('Time/s')%set x,y-axis and unit
+ylabel('Amp')
+    
